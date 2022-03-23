766277b73c350a4d8869e5ca47fe3ec0511c7a36
Marchlie <marchandlie@gmail.com>
2022-03-23 18:11:26 +0800

    feat(main): Update processing flow.

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
 import argparse
-from infoCollection import portscan
+import time
+
+import aliveTest
+import fingerprint
+import portScan
+import vulnerability
+from tools import log
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", help="Local IP", type=str)
-    parser.add_argument("-lp", help="Local port", type=int)
     parser.add_argument("t", help="Target IP", type=str)
     parser.add_argument("-tp", help="Target port", type=int)
-    parser.add_argument("-sm", help="Scan mode", type=str)
+    parser.add_argument("--debug", help="Debug mode", action="store_true")
     args = parser.parse_args()
 
-    targetIP = args.t
-    targetPort = args.tp
-    localIP = args.l
-    localPort = args.lp
-    scanMode = args.sm
+    scanConfig = {
+        "targetIP": args.t,
+        "targetPort": args.tp,
+        "isDebug": args.debug,
+    }
 
-    portscan(targetIP, targetPort, localIP, localPort, scanMode)  # 端口扫描
+    # Log ScanConfig
+    log.info("ScanConfig: " + str(scanConfig))
+
+    # Alive test
+    scanConfig = {"alive": aliveTest(scanConfig)}
+
+    # If alive, do portscan
+    if scanConfig["alive"] == True:
+        scanConfig["portlist"] = portScan(scanConfig)
+
+    # If portlist is not empty, check fingerprint
+    if scanConfig["portlist"] != None:
+        scanConfig["fingerprint"] = fingerprint(scanConfig)
+
+    # If fingerprint is not empty, check vulnerability
+
+    if scanConfig["fingerprint"] != None:
+        scanConfig["vulnerability"] = vulnerability()
+
+    # Print result
+    print(scanConfig)
+    with open(
+        "result" + time.strftime("%Y-%m-%d", time.localtime(time.time())) + ".txt",
+        "a",
+    ) as f:
+        f.write(str(scanConfig) + "\n")
 
 
 if __name__ == "__main__":
